91e3fd2c81ede9174693f2a65a55487af278a9e6
anjanipodomi <anjanidihapsari@gmail.com>
2024-05-11 11:26:29 +0700

    menambahkan keterangan

--- stackusinglinkedlist/stackusinglinkedlist.cpp
+++ stackusinglinkedlist/stackusinglinkedlist.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 using namespace std;
 
+//Node class representing a single node in the linked list
 class Node
 {
 public:
@@ -13,20 +14,22 @@ public:
 	}
 };
 
+//Stack Class
 class stack {
 private:
-	Node* top;
+	Node* top;	//pointer to the top node of the stack
 
 public:
 	stack()
 	{
-		top = NULL;
+		top = NULL;	//initialize the stack with a null top pointer
 	}
+	//push operation: Insert an element onto the top of the stack
 	int push(int value) {
-		Node* newNode = new Node();
-		newNode->data = value;
-		newNode->next = top;
-		top = newNode;
+		Node* newNode = new Node();	//1. Allocate memory for the new node
+		newNode->data = value;		//2. assign value
+		newNode->next = top;		//3. set the next pointer of the new node to the current top node
+		top = newNode;				//4. update the top pointer to the new node
 		cout << "Push value: " << value << endl;
 		return value;
 	}
